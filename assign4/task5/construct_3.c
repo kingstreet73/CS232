5430fa46019c25fd83a1e5c1aa30d8a46449b3be
kingstreet73 <eg07546n@pace.edu>
2021-02-28 22:16:11 -0500

    Second Commit(testing git commands)

--- assign4/task5/construct_3.c
+++ assign4/task5/construct_3.c
@@ -6,16 +6,25 @@ typedef struct node {
 } node_t;
 
 node_t * construct_3() {
-    //Allocate three pointers: 
+    //Allocate three pointers:  
     //x for the first Node, and temporary pointers y and z for the other two Nodes.
-
-    //Allocate three Node pointees and store references to them in the three pointers.
-
-    //Dereference each pointer to store the appropriate number into the value field in its pointee.
-
-    //Dereference each pointer to access the .next field in its pointee, 
-    //and use pointer assignment to set the .next field to point to the appropriate Node. 
-
+  node_t* x;
+	node_t* y;
+	node_t* z;
+  //Allocate three Node pointees and store references to them in the three pointers.
+  x = malloc(sizeof(node_t));
+	y = malloc(sizeof(node_t));
+  z = malloc(sizeof(node_t));
+  //Dereference each pointer to store the appropriate number into the value field in its pointee.
+  x->value=1;
+	y->value=2;
+  z->value=3;
+  //Dereference each pointer to access the .next field in its pointee, 
+  //and use pointer assignment to set the .next field to point to the appropriate Node. 
+  x->next = y;
+	y->next = z;
+	z->next = x;
+  return x;
 }
 
 //You can ignore the following code for testing
@@ -24,7 +33,7 @@ int main (int argc, char ** argv) {
 
     node_t * x = construct_3();
     return dump_all(x);
-
+    free(x);
 }
 
 int dump_all(node_t * x) {
@@ -40,4 +49,5 @@ int dump_all(node_t * x) {
         printf("%d -> %d\n", z->value, x->value);
         return 0;
     }
+    
 }
--- assign4/task6/construct_3_strs.c
+++ assign4/task6/construct_3_strs.c
@@ -6,7 +6,25 @@ typedef struct node {
 } node_t;
 
 node_t * construct_3_strs() {
-    return NULL;//just to pass compiler, please edit as needed.
+    //Allocate three pointers:  
+    //x for the first Node, and temporary pointers y and z for the other two Nodes.
+  node_t* x;
+	node_t* y;
+	node_t* z;
+  //Allocate three Node pointees and store references to them in the three pointers.
+  x = malloc(sizeof(node_t));
+	y = malloc(sizeof(node_t));
+  z = malloc(sizeof(node_t));
+  //Dereference each pointer to store the appropriate number into the value field in its pointee.
+  x->value="CS232";
+	y->value="is";
+  z->value="awesome";
+  //Dereference each pointer to access the .next field in its pointee, 
+  //and use pointer assignment to set the .next field to point to the appropriate Node. 
+  x->next = y;
+	y->next = z;
+	z->next = x;
+  return x;
 }
 
 //You can ignore the following code for testing
@@ -14,7 +32,7 @@ int dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * x = construct_3_strs();
     return dump_all(x);
-
+    free(x);  
 }
 
 int dump_all(node_t * x) {
