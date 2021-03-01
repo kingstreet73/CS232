a6320da0b272299d4094e88bfd03c57a56144cf4
kingstreet73 <eg07546n@pace.edu>
2021-02-28 22:19:08 -0500

    commit3

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -6,10 +6,23 @@ int main () {
 
   /* Zero out the array */
   int letter_frequency[26] = {0};
-  int len, i;
+  int len, i, c;
   char buf[MAX_BUF];
 
   do {
+    fgets(buf, MAX_BUF, stdin);
+    len = strlen(buf)-1;
+    for (i=0; i<len; i++){
+
+      if(buf[i] >= 'A' && buf[i] <= 'Z'){
+        c=(int)buf[i]-65;
+        letter_frequency[c]++;
+      }
+      if(buf[i] >= 'a' && buf[i] <= 'z'){
+        c=(int)buf[i]-97;
+        letter_frequency[c]++;
+      }
+    }
   } while (len > 1);
 
   printf("Distribution of letters in corpus:\n");
@@ -17,4 +30,5 @@ int main () {
     // 'A' is ASCII code 65
     printf("%c: %d\n", 65+i, letter_frequency[i]);
   }
+return 0;
 }
